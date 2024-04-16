3497a0a7ecb4cd019288b7583d8ee4ee1d287831
Axe <akashkumar.pro6@gmail.com>
2024-04-16 16:39:01 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+    <h2>About</h2>
+    
+    <p style={{marginTop: "1rem"}}>Hi, It's Akash here. I would like to become a remote front end developer</p>
+    </main>
+  )
+}
+
+export default About
